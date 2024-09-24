a3ad641393db4688326d6ea962423f7c41dbde31
Stefan Stefanov <stefanov.sm@abv.bg>
2024-09-24 14:40:51 +0300

    Update regex.sql

--- Regular expressions/regex.sql
+++ Regular expressions/regex.sql
@@ -1,20 +1,20 @@
 create temporary table a_table (id integer, address text); 
 insert into a_table 
 values
- (1, 'Ñòóäåíòñêè Êîìïëåêñ, óë. „Ðîñàðèî“ 1, 1756 Ñîôèÿ'),
- (2, 'êâ. Äèàíàáàä.19 Ã, 1172 Ñîôèÿ'),
- (3, '4000 Ïëîâäèâ Öåíòúð, óë. „Èâàí Âàçîâ“ 59'),
- (4, 'æ.ê. Ëîçåíåö, áóë. „Äæåéìñ Áàó÷åð“ 5, 1164 Ñîôèÿ');
+ (1, 'Студентски Комплекс, ул. „Росарио“ 1, 1756 София'),
+ (2, 'кв. Дианабад.19 Г, 1172 София'),
+ (3, '4000 Пловдив Център, ул. „Иван Вазов“ 59'),
+ (4, 'ж.к. Лозенец, бул. „Джеймс Баучер“ 5, 1164 София');
 
 select	id, substring(address from '\y(\d{4})\y') as zipcode
 from a_table;
 
 select * from a_table
-where address ~ 'óë\.';
+where address ~ 'ул\.';
 
 select * from a_table
-where address ~ '\yóë\.' -- note Y vs. Ó 
-   or address ~* '^ÊÂ\..*ÎÔÈß$';
+where address ~ '\yул\.' -- note Y vs. У 
+   or address ~* '^КВ\..*ОФИЯ$';
 
 select * 
 from regexp_split_to_table('Hello regexp. power - at my fingertips!', '[, \.!+-]+') as rs; 
